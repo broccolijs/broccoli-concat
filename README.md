197c0dac180845aa9b9b5fbe15302e737e93a34f
Richard Livsey <richard@livsey.org>
2014-02-19 15:22:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# broccoli-concat
+
+Concatenate trees into a single file.
+
+## Usage
+
+```js
+var concatenated = concat(sourceTree, {
+  inputFiles: [
+    'app/**/*.css'
+  ],
+  outputFile: '/assets/app.css'
+});
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var broccoli = require('broccoli')
+var Transform = require('broccoli-transform')
+
+module.exports = Concat
+
+Concat.prototype = Object.create(Transform.prototype)
+Concat.prototype.constructor = Concat
+function Concat(inputTree, options) {
+  if (!(this instanceof Concat)) return new Concat(inputTree, options)
+  this.inputTree = inputTree
+  for (var key in options) {
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key]
+    }
+  }
+
+  this.cache = {}
+}
+
+Concat.prototype.transform = function (srcDir, destDir) {
+  var self = this
+  var modulesAdded = {}
+  var output = []
+
+  // When we are done compiling, we replace this.cache with newCache, so that
+  // unused cache entries are garbage-collected
+  var newCache = {}
+
+  var inputFiles = broccoli.helpers.multiGlob(this.inputFiles, {cwd: srcDir})
+  for (i = 0; i < inputFiles.length; i++) {
+    addFile(inputFiles[i])
+  }
+
+  broccoli.helpers.assertAbsolutePaths([this.outputFile])
+  mkdirp.sync(path.join(destDir, path.dirname(this.outputFile)))
+  fs.writeFileSync(path.join(destDir, this.outputFile), output.join(''))
+
+  self.cache = newCache
+
+  function addFile (filePath) {
+    // This function is just slow enough that we benefit from caching
+    var statsHash = broccoli.helpers.hashStats(fs.statSync(srcDir + '/' + filePath), filePath)
+    var cacheObject = self.cache[statsHash]
+    if (cacheObject == null) { // cache miss
+      var fileContents = fs.readFileSync(srcDir + '/' + filePath, { encoding: 'utf8' })
+      cacheObject = {
+        output: fileContents
+      }
+    }
+    newCache[statsHash] = cacheObject
+    output.push(cacheObject.output)
+  }
+}

8df5bfe6a5f1903c0ebeadf248c9850929a92104
Edward Faulkner <ef@alum.mit.edu>
2014-12-11 02:16:38 -0500

    use normal broccoli-concat when no sourcemaps are involved

---
+++ index.js
@@ -0,0 +1,16 @@
+var sourceMapAwareConcat = require('./writer');
+var concat = require('broccoli-concat');
+
+module.exports = function(inputTree, options) {
+  if (!options && !options.outputFile) {
+    throw new Error("outputFile is required");
+  }
+  var extensions = (options.sourceMapsForExtensions || ['js']);
+  for (var i=0; i<extensions.length; i++) {
+    var ext = '.' + extensions[i].replace(/^\./,'');
+    if (options.outputFile.slice(-1 * ext.length) === ext) {
+      return sourceMapAwareConcat(inputTree, options);
+    }
+  }
+  return concat(inputTree, options);
+};

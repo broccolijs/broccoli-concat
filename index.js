1fb2168ac338da8e194c52bfa69c25f4276b0cce
Edward Faulkner <ef@alum.mit.edu>
2014-12-23 14:30:13 -0500

    dropping unneeded assertion
    we don't fall back to broccoli-concat anymore, so we don't need to
    enforce its semantics.

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var SourceMapAwareConcat = require('./concat-with-maps');
 var SimpleConcat = require('./simple-concat');
-var helpers = require('broccoli-kitchen-sink-helpers');
 
 module.exports = function(inputTree, options) {
   if (!options || !options.outputFile) {
     throw new Error("outputFile is required");
   }
-
-  // This is here because broccoli-concat does the same thing, and we
-  // don't want to surprise people by breaking only when they suddenly
-  // disable sourcemaps.
-  helpers.assertAbsolutePaths([options.outputFile]);
 
   var extensions = (options.sourceMapsForExtensions || ['js']);
   for (var i=0; i<extensions.length; i++) {

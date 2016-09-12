e73abb3d8eff37d206ebd166b26bcd20e680be62
Stefan Penner <stefan.penner@gmail.com>
2016-09-12 14:56:26 -0700

    process.env.CONCAT_SIZES enables debug output
    This output is written to disk at cwd +
    concat-stats-for-<name-of-outputfile>.json and contains the sizes of the
    various pieces the outputfile is made of.
    
    This aims to enable improved debuggability / visibility of large output
    assets.

--- concat-without-source-maps.js
+++ concat-without-source-maps.js
@@ -7,14 +7,22 @@ function Simple(attrs) {
   this._internal = '';
   this.outputFile = attrs.outputFile;
   this.baseDir = attrs.baseDir;
+  this._sizes = {};
+  this.id = attrs.pluginId;
 }
 
 Simple.prototype.addFile = function(file) {
-  this._internal += fs.readFileSync(path.join(this.baseDir, file), 'UTF-8');
+  var content =  fs.readFileSync(path.join(this.baseDir, file), 'UTF-8');
+  this._internal += content;
+  this._sizes[file] = content.length;
 };
 
 Simple.prototype.addSpace = function(space) {
   this._internal += space;
+};
+
+Simple.prototype.writeConcatStatsSync = function(outputPath, content) {
+  fs.writeFileSync(outputPath, content);
 };
 
 Simple.prototype.end = function(cb, thisArg) {
@@ -22,6 +30,19 @@ Simple.prototype.end = function(cb, thisArg) {
   if (cb) {
     result = cb.call(thisArg, this);
   }
+
+  if (process.env.CONCAT_STATS) {
+    var outputPath = process.cwd() + '/concat-stats-for-' + this.id + '-' + path.basename(this.outputFile) + '.json';
+
+    this.writeConcatStatsSync(
+      outputPath,
+      JSON.stringify({
+        outputFile: this.outputFile,
+        sizes: this._sizes
+      }, null, 2)
+    )
+  }
+
   fs.writeFileSync(this.outputFile, this._internal);
   return result;
 };

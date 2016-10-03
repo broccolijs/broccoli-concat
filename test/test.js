a016abdbafe4ff21f1e169599ccddc05a4ee2a6f
Stefan Penner <stefan.penner@gmail.com>
2016-10-03 15:55:25 -0700

    split up test files

--- test/test.js
+++ test/test.js
@@ -559,52 +559,6 @@ describe('sourcemap-concat', function() {
   });
 });
 
-describe('concat-without-maps', function() {
-  var Concat = require('../concat-without-source-maps');
-  var quickTemp = require('quick-temp');
-  var concat;
-  var outputFile;
-
-  beforeEach(function() {
-    outputFile = quickTemp.makeOrRemake(this, 'tmpDestDir') + '/' + 'foo.js';
-
-    concat = new Concat({
-      outputFile: outputFile,
-      baseDir: firstFixture
-    });
-  });
-
-  afterEach(function() {
-    quickTemp.remove(this, 'tmpDestDir');
-  });
-
-  it('addSpace', function() {
-    concat.addSpace('a');
-    concat.addSpace('b');
-    concat.addSpace('c');
-    concat.end();
-    expect(file(outputFile)).to.equal('abc');
-  });
-
-  it('addFile', function() {
-    concat.addFile('inner/first.js');
-    concat.addFile('inner/second.js');
-    concat.addFile('other/third.js');
-    concat.end();
-    expect(file(outputFile)).to.equal(file(__dirname + '/expected/concat-without-maps-1.js'));
-  });
-
-  it('addFile & addSpace', function() {
-    concat.addFile('inner/first.js');
-    concat.addSpace('"a";\n');
-    concat.addSpace('"b";\n');
-    concat.addSpace('"c";\n');
-    concat.addFile('inner/second.js');
-    concat.end();
-    expect(file(outputFile)).to.equal(file(__dirname + '/expected/concat-without-maps-2.js'));
-  });
-});
-
 function expectFile(filename) {
   var stripURL = false;
 

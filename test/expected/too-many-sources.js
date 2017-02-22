fe6efabf0573ae1798d6bc70bfe228528a48e6bf
Trent Willis <twillis@linkedin.com>
2017-02-22 10:25:51 -0800

    Port tests from fast-sourcemap-concat
    This will help ensure we have proper test coverage when landing
    the upcoming move to patch-based sourcemap concat.

---
+++ test/expected/too-many-sources.js
@@ -0,0 +1,13 @@
+function meaningOfLife() {
+  throw new Error(42);
+}
+
+function boom() {
+  throw new Error('boom');
+}
+
+
+function somethingElse() {
+  throw new Error("somethign else");
+}
+//# sourceMappingURL=too-many-sources.map

d0d0439b06cd86e46f5a218ab9dbbaceaa07e729
simon <ihmig@kaliber5.de>
2018-08-24 17:52:51 +0200

    Fix creation of empty stats directory
    We need to create the stats directory lazily as well.
    
    Fixes #136

--- test/sourcemap-concat-test.js
+++ test/sourcemap-concat-test.js
@@ -18,6 +18,7 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 const file = chaiFiles.file;
+const dir = chaiFiles.dir;
 
 const fixtures = path.join(__dirname, 'fixtures');
 const firstFixture = path.join(fixtures, 'first');
@@ -427,6 +428,19 @@ describe('sourcemap-concat', function() {
     expectValidSourcemap('sneaky.js').in(result);
   }));
 
+  it('does not create concat-stats-for directory', co.wrap(function *() {
+    let node = concat(firstFixture, {
+      outputFile: '/all-inner.js',
+      inputFiles: ['inner/*.js'],
+      sourceMapConfig: { enabled: false }
+    });
+
+    builder = new broccoli.Builder(node);
+    yield builder.build();
+
+    expect(dir(process.cwd() + '/concat-stats-for')).to.not.exist;
+  }));
+
   describe('rebuild', function() {
     let inputDir;
     let quickTemp = require('quick-temp');
@@ -646,22 +660,15 @@ describe('sourcemap-concat', function() {
       });
 
       it('emits files', co.wrap(function* () {
-        let dir = fs.statSync(dirPath);
-
-        expect(dir.isDirectory()).to.eql(true);
-        expect(walkSync(dirPath)).to.eql([]);
-
         builder = new broccoli.Builder(node);
-
-        dir = fs.statSync(dirPath);
-        expect(dir.isDirectory()).to.eql(true);
-
         yield builder.build();
-        expect(walkSync(dirPath)).to.eql([]);
+
+        expect(dir(dirPath)).to.not.exist;
 
         process.env.CONCAT_STATS = true;
-
         yield builder.build();
+
+        expect(dir(dirPath)).to.exist;
         expect(walkSync(dirPath)).to.eql([
           node.id + '-rebuild.js.json',
           node.id + '-rebuild.js/',
